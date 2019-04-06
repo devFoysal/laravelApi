44fcb69757a6710f685699028391f640d1b26f60
Foysal Mahmud <foysal.km68@gmail.com>
2019-04-07 00:59:23 +0600

    Review resource and Product pagination

--- app/Http/Resources/Product/ProductCollection.php
+++ app/Http/Resources/Product/ProductCollection.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Resources\Product;
 
-use Illuminate\Http\Resources\Json\Resource;
+use Illuminate\Http\Resources\Json\JsonResource;
 
-class ProductCollection extends Resource
+class ProductCollection extends JsonResource
 {
     /**
      * Transform the resource collection into an array.
@@ -18,6 +18,7 @@ class ProductCollection extends Resource
             'name' => $this->name,
             'totalPrice' => round((1 - ($this->discount / 100)) * $this->price, 2),
              'rating' => $this->reviews->count() > 0 ?round($this->reviews->sum('star') / $this->reviews->count(), 2):'No rating yet',
+             'discount' => $this->discount,
              'href' => [
                 'link' => route('products.show', $this->id)
              ]
